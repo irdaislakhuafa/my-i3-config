664e85a95998d48b3cbe1669df7968b85de023c9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-04-11 21:37:39 +0700

    feat: gamma command disabled
    Disables the gamma command in the autostart script, improving performance.

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -8,7 +8,7 @@ feh --bg-scale /media/My_Drive/My_Config/wallpaper/wallpaper.* &!
 
 
 # To set Gamma
-xgamma -rgamma 0.40 -ggamma 0.50 -bgamma 0.80 &!
+#xgamma -rgamma 0.40 -ggamma 0.50 -bgamma 0.80 &!
 
 # set display more colorful
 #xrandr --output eDP-1 --brightness 1.2
